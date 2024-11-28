f900464c4c754250aff1c92fc9520ec391f8db32
henak1 <elhennavi@yandex.ru>
2024-11-28 22:00:52 +0300

    небольшие изменения в routers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :profiles
-  root "profiles#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
