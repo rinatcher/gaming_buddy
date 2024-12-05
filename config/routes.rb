78743e15d88fa4f4eb31d512546b5e76b2c347d4
Dinisynch <dinisynch@mail.ru>
2024-12-05 16:11:33 +0300

    Add authorization for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+ end
+
+ resource :user, only: [:show, :edit, :update]
   root "spa#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :profiles
